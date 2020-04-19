8682ee3eb3882921d0c9034dabf29cf5167485e3
Christian Pardo <chrispardopersonal@gmail.com>
2020-04-19 17:12:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
       song
   end
   
-  def self.create_from_file_name
+  def self.create_from_file_name(file_name)
       song = self.new 
       song.name = file_name.split(" - ")[1].split(".")[0]
       song.artist_name = file_name.split(" - ")[0]
